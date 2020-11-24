05208400a4a333d7705d338e0d337626acadc401
Myra James <Treesmellgood@gmail.com>
2020-11-23 22:46:09 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>GET IT DONE NAO</h1>
+        <h2>THE APP TO HELP YOU ORGANIZE YOUR LIFE, GET STUFF DONE, AND ACHIEVE YOUR GOALS!!!!</h2>
+      </div>
+    );
+  }
+}
